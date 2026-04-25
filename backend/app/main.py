4ea3f78810a3b231b125e901376154efe89327ae
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-04-26 00:36:49 +0500

    feat: implement packet sniffing API endpoints for start and stop monitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from scapy.all import AsyncSniffer, IP, TCP, UDP, ICMP
 app = FastAPI()
 
 app.add_middleware(
@@ -9,8 +10,26 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=True,
 )
+sniffer_instance = None 
+captured_packets = []
 
-@app.get('/health')
-def health() :
-    return {"message" : "I love you"}
+def packet_callback(pkt) :
+    pass
 
+@app.post('/api/start')
+def start_monitoring() :
+    global sniffer_instance
+    if sniffer_instance is None and not sniffer_instance.running():
+        sniffer_instance = AsyncSniffer(prc=packet_callback , store=False)
+    # this store = False saves the memory otherwise it will store it in memory
+        sniffer_instance.start()
+    return {"message" : "Packet Sniffing stated ...."}
+
+@app.post('/api/stop')
+def stop_monitoring():
+    global sniffer_instance
+    if sniffer_instance and sniffer_instance.running():
+        sniffer_instance.stop()
+    return {"message" : "Packet Sniffing stopped ..."}
+
+@app.post()
