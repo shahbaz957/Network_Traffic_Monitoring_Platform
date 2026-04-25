647de55f3faad7b562151789f0fe27af4d99b26e
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-04-26 00:41:51 +0500

    feat: enhance packet callback function to capture and store packet details

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import time
 from scapy.all import AsyncSniffer, IP, TCP, UDP, ICMP
 app = FastAPI()
 
@@ -14,7 +15,32 @@ sniffer_instance = None
 captured_packets = []
 
 def packet_callback(pkt) :
-    pass
+    """This function runs every time a packet is caught"""
+    if IP in pkt:
+        # Determine Protocol
+        proto = "OTHER"
+        if TCP in pkt: proto = "TCP"
+        elif UDP in pkt: proto = "UDP"
+        elif ICMP in pkt: proto = "ICMP"
+        
+        # Determine Service (Simple Port Mapping)
+        port = pkt.sport if TCP in pkt or UDP in pkt else None
+        service = "Unknown"
+        if port == 80: service = "HTTP"
+        elif port == 443: service = "HTTPS"
+        elif port == 53: service = "DNS"
+
+        packet_info = {
+            "time": time.strftime("%H:%M:%S"),
+            "src": pkt[IP].src,
+            "dst": pkt[IP].dst,
+            "protocol": proto,
+            "size": len(pkt),
+            "service": service
+        }
+        captured_packets.append(packet_info)
+
+## for each packet the above function will run 
 
 @app.post('/api/start')
 def start_monitoring() :
@@ -32,4 +58,12 @@ def stop_monitoring():
         sniffer_instance.stop()
     return {"message" : "Packet Sniffing stopped ..."}
 
-@app.post()
+@app.post('/api/reset')
+def reset():
+    global captured_packets
+    captured_packets.clear()
+    return {"message" : "Data is reset"}
+
+@app.get("/api/packets")
+def get_packet():
+    return captured_packets[-30:] # send the latest 30 packets from the captured ones 
